d0c23ae51a556bf33e0a1c94c8536f20084a2d84
Varunendra Pratap Singh <ovarunendra@zeomega.com>
2014-12-07 12:17:25 +0530

    fixed: can not submit empty title

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -4,6 +4,9 @@ app.controller('NavCtrl', function ($scope, $location, Post) {
     $scope.post = {url: 'http://', title: ''};
 
     $scope.submitPost = function () {
+        if($scope.post.title === ''){
+            return;
+        }
         Post.create($scope.post).then(function (ref) {
             $location.path('/posts/' + ref.name());
             $scope.post = {url: 'http://', title: ''};
