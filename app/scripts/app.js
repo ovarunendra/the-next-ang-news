f66e798eb37d4c6f7a5a46132a4ff6896b0c97e6
Varunendra Pratap Singh <ovarunendra@zeomega.com>
2014-12-07 12:44:34 +0530

    added Auth Service for register and login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,32 +10,50 @@
  * Main module of the application.
  */
 var app = angular
-  .module('theNextAngNewsApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'firebase'
-  ])
-  .constant('FIREBASE_URL', 'https://thenextangnewsapp.firebaseio.com/');
+    .module('theNextAngNewsApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'firebase'
+    ])
+    .constant('FIREBASE_URL', 'https://thenextangnewsapp.firebaseio.com/');
 app.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/posts.html',
-        controller: 'PostsCtrl'
-      })
-      .when('/posts/:postId', {
-        templateUrl: 'views/showpost.html',
-        controller: 'PostViewCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        .when('/', {
+            templateUrl: 'views/posts.html',
+            controller: 'PostsCtrl'
+        })
+        .when('/posts/:postId', {
+            templateUrl: 'views/showpost.html',
+            controller: 'PostViewCtrl'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'AboutCtrl'
+        })
+        .when('/register', {
+            templateUrl: 'views/register.html',
+            controller: 'AuthCtrl',
+            resolve: {
+                user: function(Auth) {
+                    return Auth.resolveUser();
+                }
+            }
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'AuthCtrl',
+            resolve: {
+                user: function(Auth) {
+                    return Auth.resolveUser();
+                }
+            }
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
