a9579de52695b981a0e6d77b9a9e3b23c159f6b1
Varunendra Pratap Singh <ovarunendra@zeomega.com>
2014-12-07 16:17:37 +0530

    feature: add comments

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -33,13 +33,11 @@ app.factory('Auth', function ($firebaseSimpleLogin, FIREBASE_URL, $rootScope, $f
     };
 
     $rootScope.$on('$firebaseSimpleLogin:login', function(e, user) {
-        angular.copy(user, Auth.user);
         Auth.user.profile = $firebase(ref.child('profile').child(Auth.user.uid)).$asObject();
 
         console.log(Auth.user);
     });
     $rootScope.$on('$firebaseSimpleLogin:logout', function() {
-        console.log('logged out');
 
         if(Auth.user && Auth.user.profile) {
             Auth.user.profile.$destroy();
